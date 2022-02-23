0c2801023ab0a4bf6df355f90357078d2f1f151a
unknown <edward.porras@labinfo.is.escuelaing.edu.co>
2022-02-23 16:23:34 -0500

    heroku

--- src/main/java/edu/escuelaing/arem/Clima.java
+++ src/main/java/edu/escuelaing/arem/Clima.java
@@ -1,18 +1,16 @@
 package edu.escuelaing.arem;
 
+import java.net.MalformedURLException;
 import java.net.URL;
 
 public class Clima {
 
 
-    public void clima(){
+    public void clima() throws MalformedURLException {
 
-        URL url = new URL("api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}")
+        URL url = new URL("api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}");
 
 
     }
-
-
-
 
 }
