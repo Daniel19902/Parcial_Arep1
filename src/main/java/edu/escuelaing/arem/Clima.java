b6fa4affd0446c45adb27f650852fb6026bb4a58
unknown <edward.porras@labinfo.is.escuelaing.edu.co>
2022-02-23 16:18:33 -0500

    heroku

--- src/main/java/edu/escuelaing/arem/Clima.java
+++ src/main/java/edu/escuelaing/arem/Clima.java
@@ -1,6 +1,16 @@
 package edu.escuelaing.arem;
 
+import java.net.URL;
+
 public class Clima {
+
+
+    public void clima(){
+
+        URL url = new URL("api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}")
+
+
+    }
 
 
 
--- src/main/resources/public/clima.js
+++ src/main/resources/public/clima.js
@@ -4,7 +4,7 @@ var clima = (function(){
 
         api : function(pais){
             alert("entre")
-            fetch('http://localhost:4567')
+            fetch('http://localhost:4567/consulta')
                   .then(response => response.json())
                   .then(json => console.log(json))
 
