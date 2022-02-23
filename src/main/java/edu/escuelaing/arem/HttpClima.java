0d4e585cc901030f22458e67dbf15007970c06f4
clases clases <clases@labinfo.is.escuelaing.edu.co>
2022-02-23 15:46:29 -0500

    puerto

--- src/main/java/edu/escuelaing/arem/HttpClima.java
+++ src/main/java/edu/escuelaing/arem/HttpClima.java
@@ -17,11 +17,11 @@ public class HttpClima
     public static void main(String[] args) throws IOException {
         ServerSocket serverSocket = null;
         try {
-            System.out.println("Listo para recibir en el puerto:" + 36000);
-            serverSocket = new ServerSocket(5000);
+            System.out.println("Listo para recibir en el puerto:" + getPort());
+            serverSocket = new ServerSocket(getPort());
 
         }catch (IOException e){
-            System.err.println("Could not listen on port:"+36000);
+            System.err.println("Could not listen on port:"+getPort());
             System.exit(1);
         }
         while (true){
@@ -68,6 +68,13 @@ public class HttpClima
             clienteSocket.close();
         }
 
+    }
+
+    public static int getPort(){
+        if (System.getenv("PORT") == null){
+            return Integer.parseInt(System.getenv("PORT"));
+        }
+        return 4567;
     }
 
 
